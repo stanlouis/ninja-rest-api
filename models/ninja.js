0e727558e06ceea609cea5a232645642cb682a6e
Stanley Louis <stanlouis@msn.com>
2018-12-24 10:05:50 -0500

    add mongoose schema and model.

---
+++ models/ninja.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const NinjaSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name field is required']
+  },
+  rank: {
+    type: String
+  },
+  available: {
+    type: Boolean,
+    default: false
+  }
+
+  // add in geolocation
+});
+
+const Ninja = mongoose.model('ninja', NinjaSchema);
+
+module.exports = Ninja;
